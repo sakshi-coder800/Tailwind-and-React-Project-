fc780a3ed5aabc4957586dd5a7d2a846ae88fe71
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-10-01 21:04:53 +0530

    Tailwind , react project setup

---
+++ TailwindProject/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+
+      <h1 className="text-3xl font-bold underline bg">
+      Hello world!
+    </h1>
+
+    </div>
+  )
+}
+
+export default App
+
